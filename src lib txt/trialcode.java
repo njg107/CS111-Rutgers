6397b5a672baf0625fdfa1918e5a2e0ab1265703
njg107 <77369711+njg107@users.noreply.github.com>
2021-01-12 22:00:36 -0500

    Add files via upload

---
+++ src lib txt/trialcode.java
@@ -0,0 +1,7 @@
+public class trialcode {
+    public static void main(String[] args) {
+        double[] x = {0.5,0.0,1.0};
+        double[] y = {Math.sqrt(3)/2.0,0.0,0.0};
+        StdDraw.filledPolygon(x, y);
+    }
+}
